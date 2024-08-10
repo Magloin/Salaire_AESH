9c7e2a16bce69653b1011e86010a9ec61026db42
Magloin <108924373+Magloin@users.noreply.github.com>
2024-08-10 10:13:57 +0200

    modif html

--- src/components/parameters/CodePostal.jsx
+++ src/components/parameters/CodePostal.jsx
@@ -3,14 +3,13 @@ import InputSelection from "../base/InputSelection"
 
 
 
-function CodePostal () {
+function CodePostal ({textValue}) {
 
-
+    let textValue = "Code postal établissement"
 
     return (<Card>
             <h2 className=" text-center font-extrabold">Saisie des Informations</h2>
-            
-            <InputSelection postalValue={"Code postal établissement"}/>
+            <InputSelection postalValue={`${textValue}`}/>
         </Card>
     )   
 }
