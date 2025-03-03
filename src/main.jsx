48e27dff4485663b6448a3a9332a6b32bdb03f55
Magloin <108924373+Magloin@users.noreply.github.com>
2025-03-03 15:52:53 +0100

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/next';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 )
