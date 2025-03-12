3a6d145f0beedab9356300eaeb743670018bcd22
Magloin <108924373+Magloin@users.noreply.github.com>
2025-03-12 18:21:56 +0100

    Allowanves add

--- src/components/base/ContactUs.jsx
+++ src/components/base/ContactUs.jsx
@@ -1,15 +1,13 @@
 import Card from "./Card";
 
 function ContactUs () {
-return (<div className="hidden">
+return (<div className="">
   <Card>
-    <h2 className="text-lg text-left pl-1.5 font-extrabold max-sm:text-xs md:text-sm lg:text-xl xl:text-2xl xl:pl-3 ">Un soucis ? une question ?</h2>
-    <div className=" flex flex-col justify-around items-center pb-2">
-        <button class="inline-block px-7 py-1.5 overflow-hidden text-xs font-semibold transition-transform rounded-full group text text-green-700/70 bg-gray-400/30 hover:bg-gray-500/70 hover:text-white">
-          <span before="Ecrivez Nous" class="relative py-1.5 transition-transform inline-block before:content-[attr(before)] before:py-2 before:absolute before:top-full group-hover:-translate-y-full"><img class="h-10 w-20 object-contain ..." src="../Pictures/email.png" /></span>
-        </button>
-      </div> 
-    </Card>
+    <h2 className="text-lg text-left pl-1.5 font-extrabold max-sm:text-xs md:text-sm lg:text-lg xl:text-2xl xl:pl-3 ">Une question ?</h2>
+    <div className="flex justify-center">
+      <button type="button" className="focus:outline-none  bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg  max-sm:px-2 max-sm:py-1 px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 flex justify-center"><a href="mailto:help.simulateur.aesh@gmail.com" className="text-white max-sm:text-xs max-sm:font-normal md:text-sm lg:text-sm xl:text-xl font-bold" >Contactez-moi !</a></button>
+    </div>
+  </Card>
 </div>)
 }
 
